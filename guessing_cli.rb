0607db9c944b1076c53823b7ed32b8692f3e8bb2
George Gore <georgelgore@gmail.com>
2017-10-23 12:52:31 -0400

    first commit

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,19 @@
 # Code your solution here!
+def run_guessing_game
+  loop do
+
+    puts "Guess a number between 1 and 6."
+
+      guess = gets.chomp
+      num = rand(1..6)
+
+      if guess == "exit"
+        break
+      elsif guess.to_i == num
+        puts "You guessed the correct number!"
+      elsif guess.to_i != num
+        puts  "The computer guessed #{num}."
+      end
+    end
+    puts "Goodbye!"
+end
